5c9b2a8ba52ae649990471885dd650210217f4de
Robel250 <robelbiniyam0@gmail.com>
2024-09-24 20:52:16 +0300

    Add files via upload

---
+++ lab1.js
@@ -0,0 +1,11 @@
+ var num1=5;
+ var num2=6;
+ var num3=7;
+ var num4=(num1+num2+num3)/2;
+ var area=Math.sqrt(num4*(num4-num1)*(num4-num2)*(num4-num3));
+console.log(area);
+
+
+console.log(area)
+var string="class"
+console.log("javascript"+string)
